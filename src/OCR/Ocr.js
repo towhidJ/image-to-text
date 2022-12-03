5ae750466366f5f92d819d9d9ae2b8b34e393778
Towhidul islam <towhidulislamcse@gmail.com>
2022-12-03 10:58:17 +0600

    ocr

---
+++ src/OCR/Ocr.js
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import Tesseract from 'tesseract.js'
+import '../App.css'
+
+function Ocr() {
+    const [isLoading, setIsLoading] = useState(false)
+    const [image, setImage] = useState('')
+    const [text, setText] = useState('')
+    const [progress, setProgress] = useState(0)
+
+    const handleSubmit = () => {
+        setIsLoading(true)
+        Tesseract.recognize(image, ['ben', 'eng'], {
+            logger: (m) => {
+                console.log(m)
+                if (m.status === 'recognizing text') {
+                    setProgress(parseInt(m.progress * 100))
+                }
+            },
+        })
+            .catch((err) => {
+                console.error(err)
+            })
+            .then((result) => {
+                console.log(result.data)
+                setText(result.data.text)
+                setIsLoading(false)
+            })
+    }
+
+    return (
+        <div className="container">
+            <div className="row h-100">
+                <div className="col-md-5 mx-auto h-100 d-flex flex-column justify-content-center">
+                    {!isLoading && (
+                        <h1 className="text-center py-5 mc-5">Image To Text</h1>
+                    )}
+                    {isLoading && (
+                        <>
+                            <progress
+                                className="form-control"
+                                value={progress}
+                                max="100"
+                            >
+                                {progress}%{' '}
+                            </progress>{' '}
+                            <p className="text-center py-0 my-0">
+                                Converting:- {progress} %
+                            </p>
+                        </>
+                    )}
+                    {!isLoading && (
+                        <>
+                            <input
+                                type="file"
+                                onChange={(e) =>
+                                    setImage(
+                                        URL.createObjectURL(e.target.files[0])
+                                    )
+                                }
+                                className="form-control mt-5 mb-2"
+                            />
+                            <input
+                                type="button"
+                                onClick={handleSubmit}
+                                className="btn btn-primary mt-5"
+                                value="Convert"
+                            />
+                        </>
+                    )}
+                    {!isLoading && text && (
+                        <>
+                            <textarea
+                                className="form-control w-100 mt-5"
+                                rows="20"
+                                cols="80"
+                                value={text}
+                                onChange={(e) => setText(e.target.value)}
+                            ></textarea>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Ocr
